0625ca46626a6057eb7fd9dad927757ca0dd6716
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2025-02-10 15:33:10 +0800

    Added new contents

--- main.js
+++ main.js
@@ -3,10 +3,43 @@ import contentString from './content.yaml?raw';
 
 try {
   const content = yaml.load(contentString);
+
+  const profileElements = {
+    'profile-name': 'name',
+    'profile-image-src': 'image-src',
+    'profile-title': 'title',
+    'profile-title-alt': 'title-alt',
+    'profile-summary': 'summary'
+  };
+
+  for (const elementId in profileElements) {
+    const property = profileElements[elementId];
+    const element = document.getElementById(elementId);
+    if (element) {
+      if (elementId === 'profile-image-src') {
+        element.src = content.profile[property];
+      } else {
+        element.textContent = content.profile[property];
+      }
+    }
+  }
+
+  const callToAction = document.getElementById('call-to-action');
+
+  if (callToAction) {
+    callToAction.textContent = content['call-to-action'].label;
+
+    callToAction.addEventListener('click', () => {
+      if (content['call-to-action'] && content['call-to-action'].link) {
+          window.location.href = content['call-to-action'].link;
+      } else {
+          console.error("Link is missing for call to action.");
+      }
+    });
+  }
+
   const projects = content.projects;
   const projectsContainer = document.getElementById('projects-container');
-
-  document.getElementById('about-me').textContent = content['about-me'];
 
   projects.forEach(project => {
     let buttonsHTML = '';
@@ -21,7 +54,7 @@ try {
     }
 
     const cardHTML = `
-      <div class="card bg-base-100 image-full w-84 lg:w-96 h-64 shadow-sm snap-always snap-center">
+      <div class="card bg-base-100 image-full w-80 lg:w-96 h-64 shadow-sm snap-always snap-center">
         <figure>
           <img src="${project['thumbs-src']}" />
         </figure>
@@ -41,6 +74,7 @@ try {
     `;
     projectsContainer.innerHTML += cardHTML;
   });
+
 } catch (error) {
   console.error('Error parsing or processing YAML:', error);
 }
